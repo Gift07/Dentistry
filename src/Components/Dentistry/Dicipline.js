7eb32c7fd89ad27a4bea82da69f7ca1c1ea72c25
Gift07 <giftedwards9@gmail.com>
2022-10-21 07:50:25 +0300

    neweer commiy

--- src/Components/Dentistry/Dicipline.js
+++ src/Components/Dentistry/Dicipline.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Dicipline = () => {
   return (
-    <div className="pt-8 md:pt-12 lg:pt-16 pl-6 md:px-16 lg:px-24 bg-[#1e1e1e] pb-3">
+    <div className="pt-8 md:pt-12 lg:pt-16 md:px-16 lg:px-24 bg-[#1e1e1e] pb-3">
       <div className="w-full">
-        <div className=" flex items-center gap-x-2 text-white text-3xl md:text-3xl lg:text-5xl  font-bold">
+        <div className="pl-6 flex items-center gap-x-2 text-white text-3xl md:text-3xl lg:text-5xl  font-bold">
           <h1>Choose</h1>
           <h1 className="text-[#02ffdd]">Dicipline</h1>
         </div>
@@ -14,26 +14,28 @@ const Dicipline = () => {
           Dentistry
         </button>
       </div>
-      <div className="w-full flex items-center justify-center my-6 gap-x-3 px-2">
-        <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
-          Arts
-        </button>
-        <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
-          Dentist
-        </button>
-        <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
-          Hygiene
-        </button>
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-11/12 flex items-center justify-center mb-4  gap-x-3">
+          <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
+            Arts
+          </button>
+          <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
+            Dentist
+          </button>
+          <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
+            Hygiene
+          </button>
+        </div>
+        <div className="w-11/12 flex items-center  gap-x-3 px-2">
+          <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
+            Music
+          </button>
+          <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
+            Drama
+          </button>
+        </div>
       </div>
-      <div className="w-full flex items-center my-6 gap-x-3 px-2">
-        <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
-          Music
-        </button>
-        <button className="w-1/3 py-3 text-white font-semibold bg-[#0b0b0b] shadow-2xl rounded-full">
-          Drama
-        </button>
-      </div>
-      <div className="my-3">
+      <div className="my-3 pl-6">
         <div className="w-full flex items-center gap-x-2 text-sm">
           <h1 className="text-[#02ffdd] uppercase font-semibold">Note: </h1>
           <h1 className="text-white text-xs">
